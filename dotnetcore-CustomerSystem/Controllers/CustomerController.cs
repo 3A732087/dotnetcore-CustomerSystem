6618dc8a8c355330852e63842dc0c48af64d2dca
3A732087 <quannn0180716v@gmail.com>
2023-03-16 22:37:20 +0800

    Controller建立、顯示所有客戶資料

---
+++ dotnetcore-CustomerSystem/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using dotnetcore_CustomerSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetcore_CustomerSystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        CustomerSystemContext db = new CustomerSystemContext();
+
+        public IActionResult Index()
+        {
+            var customers = db.Customer.ToList();
+            return View(customers);
+        }
+    }
+}
